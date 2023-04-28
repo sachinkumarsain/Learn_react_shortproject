17593a1f853528d4ad0e55464e4687bbd3d54acd
sachin <sachinsain21082004@gmail.com>
2023-04-28 21:53:28 +0530

    icon create

--- src/Tesla/Header.js
+++ src/Tesla/Header.js
@@ -1,31 +1,33 @@
 import React from 'react'
-import logo from'./images/logo.svg'
+import logo from './images/logo.svg'
 
 function Header() {
   return (
     <>
-    <header>
+      <header>
         <div>
-            <img src={logo}></img>
+          <img src={logo}></img>
         </div>
         <div>
           <ul>
-            <li>Model S</li>
-            <li>Model 3</li>
-            <li>Model X</li>
-            <li>ModelY</li>
-            <li>Solar Roof</li>
-            <li>Solar Panels</li>
+            <li><a href=''>Model S</a></li>
+            <li><a href=''>Model 3</a></li>
+            <li><a href=''>Model X</a></li>
+            <li><a href=''>Model Y</a></li>
+            <li><a href=''>Solar Roof</a></li>
+            <li><a href=''>Solar Panels</a></li>
+
           </ul>
         </div>
         <div>
           <ul>
-            <li>Shop</li>
-            <li>Account</li>
-            <li>Menu</li>
+            <li><a href=''>Shop</a></li>
+            <li><a href=''>Account</a></li>
+            <li><a href=''>Menu</a></li>
+
           </ul>
         </div>
-    </header>
+      </header>
     </>
   )
 }
