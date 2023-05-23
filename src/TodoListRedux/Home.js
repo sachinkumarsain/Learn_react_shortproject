d4325261262f8cc150cea662a87186e0a7312789
sachin <sachinsain21082004@gmail.com>
2023-05-23 18:03:11 +0530

    home.css work and checkbox work

--- src/TodoListRedux/Home.js
+++ src/TodoListRedux/Home.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import "./home.css"
 function Home() {
   const dispatch = useDispatch()
   const intial= useSelector((state) => {
@@ -32,16 +33,17 @@ function Home() {
   }
   return (
     <>
+    <div className='container'>
       <form  onSubmit={handleSubmit} autoFocus             >
         <input autoFocus type='text' placeholder='enter name' value={intial.input} onChange={(e) => dispatch(setInput(e.target.value))} >
         </input>
         <button type='submit'>add task</button>
       </form>
-      <ul>
+      <ol>
         {
           intial.tasks.map((task,index)=>{
             return(
-              <li key={index} className={intial.input.includes(index)?"completed":""}>{task}
+              <li key={index} className={intial.complet.includes(index)?"completed":""}>{task}
               <a onClick={(e)=>handleDelete(e,index)} href=''><DeleteIcon/></a>
               <a onClick={(e)=>handleEdit(e,index)} href=''><EditIcon/></a>
               <a onClick={(e)=>handleCheck(e,index)} href=''><CheckBoxIcon/></a>
@@ -49,7 +51,8 @@ function Home() {
             )
           })
         }
-      </ul>
+      </ol>
+      </div>
     </>
   )
 }
--- src/TodoListRedux/Slices/TodoListSlice.js
+++ src/TodoListRedux/Slices/TodoListSlice.js
@@ -1,52 +1,59 @@
 
 import { createSlice } from "@reduxjs/toolkit";
- export const todoSlice=createSlice({
-    name:"todo",
-    initialState:{
-        input:"",
-        tasks:[],
-        editing:false,
-        complet:[]
+export const todoSlice = createSlice({
+    name: "todo",
+    initialState: {
+        input: "",
+        tasks: [],
+        editing: false,
+        complet: []
 
     }
     ,
-    reducers:{
-        setInput:function(state,action){
-            state.input=action.payload
+    reducers: {
+        setInput: function (state, action) {
+
+            state.input = action.payload
+
         },
-        setTasks:function(state,action){   
-            if(state.editing !== false){
+        setTasks: function (state, action) {
+            if (state.editing !== false) {
                 state.tasks[state.editing] = state.input
                 state.editing = false
-            }else{
-                state.tasks=[...state.tasks,state.input]
                 state.input=""
+            } else {
+                if (state.input.length > 0) {
+                    state.tasks = [...state.tasks, state.input]
+                    state.input = ""
+                }
             }
         },
-        deleteTask:function (state,action){
-            state.tasks=state.tasks.filter((task,index)=>{
-                return index!==action.payload
+        deleteTask: function (state, action) {
+            state.tasks = state.tasks.filter((task, index) => {
+                return index !== action.payload
             })
-            state.complet=state.complet.filter((task,index)=>{
-                if(index===action.payload){
+            state.complet = state.complet.filter((task, index) => {
+                if (index === action.payload) {
                     return false
                 }
-                else{
+                else {
                     return true
                 }
             })
         },
-        editTask:function (state,action){
-      
-            state.editing=action.payload
-            state.input=state.tasks[action.payload]
+        editTask: function (state, action) {
+
+            state.editing = action.payload
+            state.input = state.tasks[action.payload]
+
         },
 
-        checkTask:function (state,action){
-             state.complet=[...state.complet,action.payload]
+        checkTask: function (state, action) {
+            state.complet = [...state.complet, action.payload]
+            //  console.log(action.payload)
         }
 
     }
- })
- export const {setInput,setTasks,deleteTask,editTask,checkTask}= todoSlice.actions
- export default todoSlice.reducer
+})
+export const { setInput, setTasks, deleteTask, editTask, checkTask } = todoSlice.actions
+export default todoSlice.reducer
