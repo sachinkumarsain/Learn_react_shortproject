dfa3f74a49ce7d38cbcb5319b4f8203bed3c27dc
sachin <sachinsain21082004@gmail.com>
2023-08-07 10:27:50 +0530

    typeCode api work

--- src/TypeCode/TypeCode.js
+++ src/TypeCode/TypeCode.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function TypeCode() {
+    const [data , setData]= useState([])
+    const[inputValue , setInputValue]=useState("")
+    useEffect(()=>{
+        axios.get(` https://jsonplaceholder.typicode.com/photos/${inputValue} `)
+        .then((result)=>{
+            setData(result.data)
+        })
+    },[inputValue])
+    console.log(data)
   return (
-    <div>TypeCode</div>
+    <>
+    <div className='Wapper'>
+        <label>Put a Number</label>
+        <input placeholder='Put anumber' value={inputValue} onChange={(e)=>setInputValue(e.target.value)}></input>
+        <div className='typeCode'>
+           {
+            data.map((item)=>{
+             return <div className='typeCodeContent'>
+                    <h2>{item.title}</h2>
+              </div>
+            })
+           }
+        </div>
+    </div>
+    </>
   )
 }
 
