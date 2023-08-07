ea101388d3641eaee94c3e4afb573fcefb1aa20d
sachin <sachinsain21082004@gmail.com>
2023-08-07 14:20:58 +0530

    typeCode api work

--- src/TypeCode/TypeCode.js
+++ src/TypeCode/TypeCode.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import "./TypeCode.css"
 
 
 function TypeCode() {
@@ -14,11 +15,11 @@ function TypeCode() {
     console.log(data)
   return (
     <>
-    <div className='Wapper'>
+    <div className='wapper'>
         <label>Put a Number</label>
         <input type='number' placeholder='Put anumber' value={value} onChange={(e)=>setValue(e.target.value)}></input>
         <div className='typeCode'>
-          <h1>{`${data.id}${" : "}${data.title}`}</h1>
+          <h1><span>{data.id}</span><span>{data.title}</span></h1>
           <img src={data.url}></img> 
         </div>
     </div>
