c0101b9cfa836cbefeb97e0b3dad42af3124c503
sachinkumarsain <sachinsain21082004@gmail.com>
2023-10-18 10:03:38 +0530

    random user

--- src/RandomUser/RandowUser.js
+++ src/RandomUser/RandowUser.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function RandowUser() {
+
+    const[userData , setUserData] = useState({})
+    const[random , setRandom] = useState(false)
+
+    useEffect(()=>{
+        axios.get("https://randomuser.me/api/")
+        .then((result)=>{
+            setUserData({...result.data.results[0]})
+            setRandom(false)
+        })
+    },[random])
+
+    function handleRandom(){
+        setRandom(true)
+    }
+    console.log(userData)
+
   return (
-    <div>RandowUser</div>
+    <>
+    <div className='wapper'>
+         <div className='box'>
+            <img src={userData.picture.large} ></img>
+            <h2>{`${userData.name.title} ${userData.name.first} ${userData.name.last}`}</h2>
+            <button onClick={handleRandom}>Random User</button>
+         </div>
+    </div>
+    </>
   )
 }
 
